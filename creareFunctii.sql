38a71294787054d084b7c64d944605f17753aa94
Tiberiu-Cocei <48532599+Tiberiu-Cocei@users.noreply.github.com>
2019-04-30 18:33:38 +0300

    finished functions

--- creareFunctii.sql
+++ creareFunctii.sql
@@ -1,3 +1,12 @@
+CREATE INDEX transaction_date_index
+on transaction_history(transaction_date); 
+
+CREATE INDEX eotm
+on transaction_history(id_staff, transaction_date); 
+
+CREATE INDEX eotm2
+on staff(job_position, id_staff); 
+
 CREATE OR REPLACE FUNCTION new_customer(
     p_f_name IN VARCHAR2, p_l_name IN VARCHAR2, p_city IN VARCHAR2, p_email IN VARCHAR2, p_phone_number IN VARCHAR2, p_date_of_birth IN DATE, p_account_type IN VARCHAR2, p_value IN INT)
 RETURN VARCHAR2
@@ -70,7 +79,7 @@ END;
 /
 
 CREATE OR REPLACE FUNCTION update_staff(
-    p_id_staff IN INT, p_id_bank IN INT, p_f_name IN VARCHAR2, p_l_name IN VARCHAR2, p_city IN VARCHAR2, p_email IN VARCHAR2, p_phone_number IN VARCHAR2, p_job_position IN VARCHAR2, p_salary IN VARCHAR2)
+    p_id_staff IN INT, p_id_bank IN INT, p_f_name IN VARCHAR2, p_l_name IN VARCHAR2, p_city IN VARCHAR2, p_email IN VARCHAR2, p_phone_number IN VARCHAR2, p_job_position IN VARCHAR2)
 RETURN VARCHAR2
 AS
   v_id_bank staff.id_bank%type;
@@ -80,7 +89,6 @@ AS
   v_email staff.email%type;
   v_phone_number staff.phone_number%type;
   v_job_position staff.job_position%type;
-  v_salary staff.salary%type;
 BEGIN
   IF (p_id_staff IS NULL) THEN
     return 'Staff id cannot be null';
@@ -109,16 +117,12 @@ BEGIN
     SELECT * INTO v_job_position FROM (SELECT job_position FROM staff WHERE id_staff = p_id_staff);
     ELSE v_job_position := p_job_position;
   END IF;
-  IF (p_salary IS NULL) THEN
-    SELECT * INTO v_salary FROM (SELECT salary FROM staff WHERE id_staff = p_id_staff);
-    ELSE v_salary := p_salary;
-  END IF;
   IF (p_id_bank IS NULL) THEN
-    SELECT * INTO v_id_bank FROM (SELECT salary FROM staff WHERE id_staff = p_id_staff);
+    SELECT * INTO v_id_bank FROM (SELECT id_bank FROM staff WHERE id_staff = p_id_staff);
     ELSE v_id_bank := p_id_bank;
   END IF;
   UPDATE staff SET f_name = v_f_name, l_name = v_l_name, city = v_city, email = v_email, phone_number = v_phone_number, job_position = v_job_position,
-  id_bank = v_id_bank, salary = v_salary WHERE id_staff = p_id_staff;
+  id_bank = v_id_bank WHERE id_staff = p_id_staff;
   COMMIT;
   return 'Successfully updated staff member.';
 END;
@@ -225,13 +229,51 @@ BEGIN
 END;
 /
 
+CREATE OR REPLACE FUNCTION account_freeze(p_id_account IN INT)
+RETURN VARCHAR2
+AS
+BEGIN
+  IF (p_id_account IS NULL) THEN
+    return 'Account id cannot be null';
+  END IF;
+  UPDATE bank_account SET account_type = 'Frozen' WHERE id_account = p_id_account;
+  COMMIT;
+  return 'Given account has been successfully frozen';
+END;
+/
+
+CREATE OR REPLACE FUNCTION account_check(p_id_account IN INT)
+RETURN VARCHAR2
+AS
+  v_nr_transactions INT;
+  v_today DATE;
+BEGIN
+  IF (p_id_account IS NULL) THEN
+    return 'Account id cannot be null';
+  END IF;
+  v_today := to_date(sysdate,'DD-MON-YY');
+  SELECT * INTO v_nr_transactions FROM (SELECT COUNT(*) FROM transaction_history WHERE id_account_from = p_id_account AND transaction_date LIKE v_today);
+  IF(v_nr_transactions > 10) THEN
+  UPDATE bank_account SET account_type = 'Frozen' WHERE id_account = p_id_account;
+  COMMIT;
+  return 'Too many transactions registered today. Account has been frozen.';
+  END IF;
+  return 'Status is ok, only '||v_nr_transactions||' transactions found for today';
+END;
+/
+
 CREATE OR REPLACE FUNCTION add_transaction(
     p_id_bank IN INT, p_id_staff IN INT, p_id_account_from IN INT, p_id_account_to IN INT, p_currency IN VARCHAR2, p_transaction_type IN VARCHAR2, p_date IN DATE, p_hour IN VARCHAR2, p_money_amount IN INT)
 RETURN VARCHAR2
 AS
   v_id_transaction transaction_history.id_transaction%type;
   v_id_rate exchange_rate.id_rate%type;
+  v_msg VARCHAR2(100);
 BEGIN
+  v_msg := account_check(p_id_account_from);
+  IF (v_msg = 'Too many transactions registered today. Account has been frozen.') THEN
+    return 'Transaction failed. Account is frozen due to a suspicious number of transactions made recently.';
+  END IF;
   SELECT * INTO v_id_transaction FROM (SELECT count(*) FROM transaction_history);
   SELECT * INTO v_id_rate FROM (SELECT count(*) FROM exchange_rate);
   CASE
@@ -247,5 +289,143 @@ BEGIN
     END CASE;
   INSERT INTO transaction_history VALUES (v_id_transaction + 1, p_id_bank, p_id_staff, p_id_account_from, p_id_account_to, v_id_rate, p_transaction_type, p_date, p_hour, p_money_amount, NULL, NULL);
   return 'Transaction finished successfully';
+END;
+/
+
+CREATE OR REPLACE FUNCTION most_active_day(month_number IN INT, year_number IN INT) RETURN VARCHAR2 AS
+  TYPE varr IS VARRAY(1000) OF varchar2(255);
+  v_month VARCHAR2(4);
+  v_transaction_number INT;
+  v_transaction_total INT;
+  v_number_aux INT;
+  v_total_aux INT;
+  v_year INT;
+  v_month_list varr := varr('JAN','FEB','MAR','APR','MAY','JUN','JUL','AUG','SEP','OCT','NOV','DEC');
+  v_day INT;
+BEGIN
+  IF(year_number < 2018 OR year_number > 2050) THEN
+    return 'Invalid year';
+  END IF;
+  v_day := 0;
+  v_year := SUBSTR(year_number, 3, 4);
+  v_transaction_number := 0;
+  v_transaction_total := 0;
+  IF(month_number < 1 OR month_number > 12) THEN
+    return 'Invalid month number';
+  END IF;
+  v_month := v_month_list(month_number);
+  FOR v_i IN 1..31 LOOP
+    v_number_aux := 0;
+    v_total_aux := 0;
+    IF(v_i < 10) THEN
+      SELECT * INTO v_number_aux FROM (SELECT COUNT(*) FROM transaction_history WHERE transaction_date LIKE '0'||v_i||'-'||v_month||'-'||v_year);
+    ELSIF (v_i >= 10) THEN
+      SELECT * INTO v_number_aux FROM (SELECT COUNT(*) FROM transaction_history WHERE transaction_date LIKE v_i||'-'||v_month||'-'||v_year);
+    END IF;
+    IF (v_number_aux > v_transaction_number) THEN
+      v_transaction_number := v_number_aux;
+      v_day := v_i;
+    END IF;
+  END LOOP;
+  IF (v_day = 0) THEN return 'No transactions found';
+  ELSE
+  return 'Most active day is day number '||v_day||' with '||v_transaction_number||' total transactions made.';
+  END IF;
+END;
+/
+ 
+CREATE OR REPLACE FUNCTION monthly_money_sum(month_number IN INT, year_number IN INT) RETURN VARCHAR2 AS
+  TYPE varr IS VARRAY(1000) OF varchar2(255);
+  v_month VARCHAR2(4);
+  v_total_RON INT;
+  v_total_EUR INT;
+  v_total_RUB INT;
+  v_total_GBP INT;
+  v_year INT;
+  v_month_list varr := varr('JAN','FEB','MAR','APR','MAY','JUN','JUL','AUG','SEP','OCT','NOV','DEC');
+  v_total INT;
+BEGIN  
+  IF(year_number < 2018 OR year_number > 2050) THEN
+    return 'Invalid year';
+  END IF;
+  v_year := SUBSTR(year_number, 3, 4);
+  v_total_RON := 0;
+  v_total_EUR := 0;
+  v_total_GBP := 0;
+  v_total_RUB := 0;
+  v_total := 0;
+  IF(month_number < 1 OR month_number > 12) THEN
+    return 'Invalid month number';
+  END IF;
+  v_month := v_month_list(month_number);
+  FOR v_i IN (SELECT t.money_amount, e.initial_currency, e.to_RON FROM transaction_history t JOIN exchange_rate e ON e.id_rate = t.id_rate WHERE transaction_date LIKE '%'||v_month||'-'||v_year)
+  LOOP
+    CASE
+      WHEN v_i.initial_currency = 'GBP' THEN v_total_GBP := v_total_GBP + ABS(v_i.money_amount);
+      WHEN v_i.initial_currency = 'EUR' THEN v_total_EUR := v_total_EUR + ABS(v_i.money_amount);
+      WHEN v_i.initial_currency = 'RON' THEN v_total_RON := v_total_RON + ABS(v_i.money_amount);
+      WHEN v_i.initial_currency = 'RUB' THEN v_total_RUB := v_total_RUB + ABS(v_i.money_amount);
+    END CASE;
+    v_total := v_total + ABS(v_i.money_amount) * v_i.to_RON;
+  END LOOP;
+  IF (v_total = 0) THEN return 'No transactions found';
+  ELSE return 'In the given month and year, the RON transactions add up to '||v_total_RON||', the GBP transactions add up to '||v_total_GBP||
+  ', the EUR transactions add up to '||v_total_EUR||' and the RUB transactions add up to '||v_total_RUB||'. Converted total is '||v_total||' RON.';
+  END IF;
+END;
+/
+
+CREATE OR REPLACE FUNCTION salary_increase(
+    p_id_staff IN INT, p_increase IN INT)
+RETURN VARCHAR2
+AS
+BEGIN
+  IF (p_id_staff IS NULL) THEN
+    return 'Staff id cannot be null';
+  END IF;
+  IF (p_increase IS NULL) THEN
+    return 'The value cannot be null';
+  END IF;
+  UPDATE staff SET salary = salary + p_increase WHERE id_staff = p_id_staff;
+  COMMIT;
+  return 'Successfully increased the salary of given staff member.';
+END;
+/
+
+CREATE OR REPLACE FUNCTION employee_of_the_month(month_number IN INT, year_number IN INT, raise_value IN INT) RETURN VARCHAR2 AS
+  TYPE varr IS VARRAY(1000) OF varchar2(255);
+  v_month VARCHAR2(4);
+  v_number_of_transactions INT;
+  v_number_of_transactions_aux INT;
+  v_id_staff staff.id_staff%type;
+  v_year INT;
+  v_month_list varr := varr('JAN','FEB','MAR','APR','MAY','JUN','JUL','AUG','SEP','OCT','NOV','DEC');
+  v_msg VARCHAR2(500);
+BEGIN
+  IF(year_number < 2018 OR year_number > 2050) THEN
+    return 'Invalid year';
+  END IF;
+  IF(raise_value < 0 OR raise_value > 500 OR raise_value = NULL) THEN
+    return 'Invalid raise value';
+  END IF;
+  v_number_of_transactions := 0;
+  v_number_of_transactions_aux := 0;
+  v_id_staff := 0;
+  v_year := SUBSTR(year_number, 3, 4);
+  IF(month_number < 1 OR month_number > 12) THEN
+    return 'Invalid month number';
+  END IF;
+  v_month := v_month_list(month_number);
+  FOR v_i IN (SELECT id_staff FROM staff WHERE job_position IN ('Consultant','Teller')) LOOP
+    SELECT * INTO v_number_of_transactions_aux FROM (SELECT COUNT(*) FROM transaction_history WHERE id_staff = v_i.id_staff AND transaction_date LIKE '%'||v_month||'-'||v_year);
+    IF(v_number_of_transactions_aux > v_number_of_transactions) THEN
+      v_number_of_transactions := v_number_of_transactions_aux;
+      v_id_staff := v_i.id_staff;
+    END IF;
+  END LOOP;
+  v_msg := salary_increase(v_id_staff, raise_value);
+  IF(v_id_staff = 0) THEN return 'No transactions found in that given month and year'; 
+  ELSE return 'Employee of the month is the employee with id '||v_id_staff||' and has been successfully given a raise.';
+  END IF;
 END;
 /
